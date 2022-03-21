b9a38eeac51418be90a1d2acd45c209795573e0f
Arthur Xavier <garok102@gmail.com>
2022-03-20 21:33:20 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
     mode: 'production',
     target: 'node',
-    externals: [nodeExternals()], 
-        entry: './build/main.js', 
+    // externals: [nodeExternals()], 
+    entry: './build/main.js', 
     output: {
         path: path.join(__dirname, 'build/production'), 
         filename: 'watchdog.js',
