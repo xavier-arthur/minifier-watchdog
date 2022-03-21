19d7499f89c0d5c51514d5f9f77c8ee76d705fbd
Arthur Xavier <garok102@gmail.com>
2022-03-20 21:19:08 -0300

    building for production

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    mode: 'production',
+    target: 'node',
+    externals: [nodeExternals()], 
+        entry: './build/main.js', 
+    output: {
+        path: path.join(__dirname, 'build/production'), 
+        filename: 'watchdog.js',
+    },
+    optimization: {
+        minimize: true, 
+    },
+};
